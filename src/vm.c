14a8db16d1e210f21454019c7b3f29d753aeb704
HASUMI Hitoshi <hasumikin@gmail.com>
2021-08-27 09:19:27 +0900

    Save toplevel self from GC

--- src/vm.c
+++ src/vm.c
@@ -1361,6 +1361,7 @@ static inline int op_return( mrbc_vm *vm, mrbc_value *regs )
   FETCH_B();
 
   if (vm->flag_permanence == 1 && vm->callinfo_tail == NULL) {
+    mrbc_incref(&regs[a]);
     return -1;
   }
 
